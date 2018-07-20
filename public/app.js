f1f1a85368acd6f23d53698be02a3d1ecf5ac603
Joe Booth <joseph.booth3@gmail.com>
2018-07-20 11:35:20 -0500

    adding function to get articles

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+//on-click event to scrape NYT site
 $("#scrape").on("click", function() {
     $.ajax({
         method: "GET", url: "/scrape",
@@ -6,3 +7,11 @@ $("#scrape").on("click", function() {
         window.location = "/"
     })
 });
+
+//Grab the articles as JSON
+
+$.getJSON("/articles", function(data) {
+    for(var i =0; i < data.length; i++) {
+        $("#articles").append("<p data-id'" + data[i].id + "';" + data[i].title + "<br />" + data[i].link + "</p>");
+    }
+})
